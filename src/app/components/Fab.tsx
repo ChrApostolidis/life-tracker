c6bc6ce3308cea78dd8ac29af885f2b31b8a7fe1
Xristos <xrsapostolidis@gmail.com>
2026-06-03 22:58:11 +0300

    Add Fab component and styles for quick capture functionality

---
+++ src/app/components/Fab.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { useApp } from '@/lib/app-context';
+import styles from './fab.module.css';
+
+export default function Fab() {
+  const { openCapture } = useApp();
+  return (
+    <button className={styles.fab} onClick={openCapture} aria-label="Quick capture (Ctrl+K)">
+      <FontAwesomeIcon icon={faPlus} />
+    </button>
+  );
+}
